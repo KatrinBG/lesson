04b42ed2eb87300426520b7ab63968ce9ac049a6
ekaterina.bozunova <Katrin060189@gmail.com>
2021-11-15 18:59:28 +0300

    Add if/else

--- src/ru/home/kate/homework2/Main.java
+++ src/ru/home/kate/homework2/Main.java
@@ -5,18 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        int x = 0;
+        int y = 0;
+        int z = 0;
         int result;
         String formula = "x-(y/z)";
 
         Scanner in = new Scanner(System.in);
 
         System.out.println("Введите число x: ");
-        int x = in.nextInt();
+        if (in.hasNextInt()) {
+            x = in.nextInt();
+        } else {
+            return;
+        }
         System.out.println("Введите число y: ");
-        int y = in.nextInt();
+        if (in.hasNextInt()) {
+            y = in.nextInt();
+        } else {
+            return;
+        }
         System.out.println("Введите число z: ");
-        int z = in.nextInt();
+        if (in.hasNextInt()) {
+            z = in.nextInt();
+        } else {
+            return;
+        }
         in.close();
         System.out.println("formula: " + formula);
         result = calc(x, y, z);
